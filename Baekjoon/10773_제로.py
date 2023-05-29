5d2e5ef58d3dbf033b4b36ecb91b3c1a9f248c3a
joo <jooh9992@gmail.com>
2023-05-29 16:40:28 +0900

    백준 실버 4 스택

---
+++ Baekjoon/10773_제로.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+k = int(input())
+stack = []
+
+for i in range(k):
+    command = int(input())
+
+    if command == 0:
+        stack.pop()
+    else:
+        stack.append(command)
+print(sum(stack))
