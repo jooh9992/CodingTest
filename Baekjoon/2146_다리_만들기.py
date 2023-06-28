0a24bbd09d38a01397f0b6baf88be02a149c469e
joo <jooh9992@gmail.com>
2023-06-28 16:18:04 +0900

    백준 골드 3 BFS

--- Baekjoon/2146_다리_만들기.py
+++ Baekjoon/2146_다리_만들기.py
@@ -1,7 +1,7 @@
 import sys
 from collections import deque
 
-imput = sys.stdin.readline
+input = sys.stdin.readline
 
 n = int(input())
 graph = [list(map(int, input().split())) for _ in range(n)]
@@ -9,9 +9,11 @@ visited = [[False] * n for _ in range(n)]
 
 dir = ((0, 1), (0, -1), (1, 0), (-1, 0))
 
+#현재 위치가 범위를 벗어나거나 이미 방문한 위치인지 확인하는 함수
 def condition(ni, nj):
     return ni < 0 or ni >= n or nj < 0 or nj >= n or visited[ni][nj]
 
+#BFS
 def marking(y, x, mark):
     q = deque()
     q.append((y, x))
@@ -21,17 +23,19 @@ def marking(y, x, mark):
         i, j = q.popleft()
         for dy, dx in dir:
             ni, nj = i + dy, j + dx
-            if condition(ni, nj) or not graph[ni][nj]:   continue
-            graph[ni][nj] = mark
-            visited[ni][nj] = True
+            if condition(ni, nj) or not graph[ni][nj]:   
+                continue
+            graph[ni][nj] = mark #연결된 영역이면 mark 값으료 표시
+            visited[ni][nj] = True #방문했다고 표시
             q.append((ni, nj))
 
+#특정위치에서 다른 위치까지의 최단 거리를 구함
 def getDist(y, x, now):
     q = deque()
     q.append((y, x, 0))
     while q:
         i, j, cnt = q.popleft()
-        if graph[i][j] != 0 and graph[i][j] != now:
+        if graph[i][j] != 0 and graph[i][j] != now: #탐색과정에서 만나는 위치가 다른 영역에 속하면 끝
             return cnt
         for dy, dx in dir:
             ni, nj = i + dy, j + dx
