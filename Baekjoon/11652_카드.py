9135f0091e888c9b21b510db23e49cdf0091eed9
joo <jooh9992@gmail.com>
2023-07-30 14:45:16 +0900

    백준 실버 4 mode 함수 사용 정렬

--- Baekjoon/11652_카드.py
+++ Baekjoon/11652_카드.py
@@ -13,3 +13,15 @@ for i in range(n):
 
 dic = sorted(dic.items(), key= lambda x:(-x[1], x[0]))
 print(dic[0][0])
+
+# 최빈값을 구하는 mode 함수를 사용하는 방법
+from statistics import *
+
+n = int(input())
+arr = []
+
+for i in range(n):
+    arr.append(int(input()))
+
+arr.sort()
+print(mode(arr))
