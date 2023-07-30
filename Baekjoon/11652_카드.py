f0321b51ff6efaa20e5b58d58c7aa8dfdaa80317
joo <jooh9992@gmail.com>
2023-07-30 14:37:38 +0900

    백준 실버 4 자료구조, 정렬

---
+++ Baekjoon/11652_카드.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+dic = {}
+
+for i in range(n):
+    tmp = int(input())
+    if tmp in dic:
+        dic[tmp] += 1
+    else:
+        dic[tmp] = 1
+
+dic = sorted(dic.items(), key= lambda x:(-x[1], x[0]))
+print(dic[0][0])
