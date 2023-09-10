df5b779abd0baf6272367717d8813705d8c4e051
joo <jooh9992@gmail.com>
2023-09-10 21:17:11 +0900

    백준 실버 4 - 그리디

--- Baekjoon/11399_ATM.py
+++ Baekjoon/11399_ATM.py
@@ -13,3 +13,21 @@ for i in p:
     answer += an
 
 print(answer)
+
+#23/09/10 다시 풀기
+
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+p = list(map(int, input().split()))
+sum = 0
+result = 0
+
+p.sort()
+
+for i in p:
+    sum += i
+    result += sum
+
+print(result)
