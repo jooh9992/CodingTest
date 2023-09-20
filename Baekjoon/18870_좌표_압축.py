ba45b55a2fea8e20aea42e2834545cce4b08759c
joo <jooh9992@gmail.com>
2023-09-20 12:31:16 +0900

    백준 실버 2 - 좌표압축

---
+++ Baekjoon/18870_좌표_압축.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+arr = list(map(int, input().split()))
+
+arr2 = sorted(set(arr))
+dic = {}
+
+for i in range(len(arr2)):
+    dic[arr2[i]] = i
+
+for i in arr:
+    print(dic[i], end=' ')
