9378b57e5bb998d2f1acc814c9ccfde041add173
joo <jooh9992@gmail.com>
2024-01-02 16:42:08 +0900

    프로그래머스 Lv1 - 해시

--- Programmers/폰켓몬.py
+++ Programmers/폰켓몬.py
@@ -8,3 +8,12 @@ def solution(nums):
         answer = len(a)
     
     return answer
+
+#24/01/02
+def solution(nums):
+    answer = 0
+    
+    a = len(nums)//2
+    b = len(set(nums))
+    
+    return min(a, b)
