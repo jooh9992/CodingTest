fc592fc501e3998acc2526ac5fa5962a2b6f8f51
joo <jooh9992@gmail.com>
2024-04-20 21:09:57 +0900

    프로그래머스 Lv2 - 스택/큐

--- Programmers/다리를_지나는_트럭.py
+++ Programmers/다리를_지나는_트럭.py
@@ -20,3 +20,28 @@ def solution(bridge_length, weight, truck_weights):
                 bridge.append(0)
         
     return answer
+
+
+# 추가 테스트케이스 버전
+from collections import deque
+
+def solution(bridge_length, weight, truck_weights):
+    answer = 0
+    bridge = deque([0] * bridge_length)
+    truck_weights = deque(truck_weights)
+    weight_bridge = 0
+    
+    while len(truck_weights) != 0:
+        answer += 1
+        
+        weight_bridge -= bridge.popleft()
+        
+        if weight_bridge + truck_weights[0] <= weight:
+            weight_bridge += truck_weights[0]
+            bridge.append(truck_weights.popleft())
+        else:
+            bridge.append(0)
+    
+    answer += bridge_length
+    
+    return answer
